5cfea79bfed5acdf28736d49b8ca5102cf9aa9c2
Josh Mabry <mabry1985@gmail.com>
2021-02-08 11:26:50 -0800

    refactor to recieve title and body slots

--- modal.js
+++ modal.js
@@ -42,7 +42,7 @@ class Modal extends HTMLElement {
           padding: 1rem;
         }
 
-        header h1 {
+        ::slotted(h1) {
           font-size: 1.25rem;
         }
 
@@ -65,10 +65,10 @@ class Modal extends HTMLElement {
       <div id="backdrop"></div>
       <div id="modal">
         <header>
-          <h1>Please Confirm</h1>
+          <slot name="title"></slot>
         </header>
         <section id="main">
-          <slot></slot>
+          <slot name="body"></slot>
         </section>
         <section id="actions">
           <button>Cancel</button>
@@ -76,6 +76,11 @@ class Modal extends HTMLElement {
         </section>
       </div>
     `;
+
+    const slots = this.shadowRoot.querySelectorAll('slot');
+    slots[1].addEventListener('slotchange', event => {
+      console.dir(slots[1].assignedNodes());
+    });
   }
 
   open() {
